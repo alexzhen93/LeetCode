876ddcfafdd364070e8e2cb1d06c11ca246a35df
Alex Zhen <alex_15_zhen@hotmail.com>
2024-02-26 18:43:17 -0500

    leetcode practises

--- Array & Hashing/347. Top K Frequent Elements.py
+++ Array & Hashing/347. Top K Frequent Elements.py
@@ -10,7 +10,7 @@ class Solution:
         
         res = []
         
-        for i in range(len(freq) - 1, 0, -1):
+        for i in range(len(freq) - 1, -1, -1):
             for n in freq[i]:
                 res.append(n)
                 if len(res) == k:
